363968b1578494577b839b9a7fa67df46f933eb1
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-03-25 13:34:45 -0700

    Cleaned up CSS background code

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const css = document.querySelector('h3');
+const cssCode = document.querySelector('h3');
 const color1 = document.querySelector('.color1');
 const color2 = document.querySelector('.color2');
 const body = document.getElementById('gradient');
 
 color1.value = '#6A5ACD';
 color2.value = '#32CD32';
-css.textContent = "linear-gradient(to right, slateblue, limegreen);";
+cssCode.textContent = "linear-gradient(to right, slateblue, limegreen);";
 
 
 function changeColor() {
@@ -13,7 +13,10 @@ function changeColor() {
 	${color1.value}, 
 	${color2.value})`;
 
-	css.textContent = body.style.background + ';';
+	newBackground = body.style.background.slice(17, -20);
+	
+
+	cssCode.textContent = newBackground + ';';
 }
 
 
