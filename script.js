d2d091023c06aa9bf0c20b2f92eada56c4088813
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-04-21 19:28:01 -0700

    Working out bug 2

--- script.js
+++ script.js
@@ -7,17 +7,17 @@ const body = document.getElementById('gradient');
 
 color1.value = '#6A5ACD';
 color2.value = '#32CD32';
-cssCode.textContent = "linear-gradient(to right, slateblue, limegreen);";
+cssCode.innerText = "linear-gradient(to right, slateblue, limegreen);";
 
 
 function changeColor() {
 	body.style.background = `linear-gradient(to right, 
 	${color1.value}, 
-	${color2.value})`;
+	${color2.value});`;
 
-	newBackground = body.style.background + ';';
+	newBackground = body.style.background;
 
-	cssCode.textContent = newBackground;
+	cssCode.innerText = newBackground;
 }
 
 
